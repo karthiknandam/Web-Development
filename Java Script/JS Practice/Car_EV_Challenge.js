7dc3f3a9e5731306dc06c95b42cb9db74652ec46
Karthik Nandam <140495326+karthiknandam@users.noreply.github.com>
2024-08-19 16:00:59 +0530

    Add files via upload

--- Java Script/JS Practice/Car_EV_Challenge.js
+++ Java Script/JS Practice/Car_EV_Challenge.js
@@ -33,3 +33,53 @@ MHEV.accelerate();
 // -> this here accelerate will exicicute the value of the call class in which we called ev according to the java stack method 
 MHEV.break();
 console.log(MHEV);
+
+
+
+// ^ -> Challenge #4 Car and EV class by implimenting chaining methods and making private charge method here ;
+
+
+class Carcl {
+    constructor(name , speed){
+        this.name= name ;
+        this.speed = speed;
+    }
+
+    accelerate(){
+        this.speed += 10;
+        console.log(`You are moving at a speed of ${this.speed}`);
+        return this;
+    }
+
+    break(){
+        this.speed -=5;
+        console.log(`You are moving at a speed of ${this.speed}`);
+        return this;
+    }
+}
+
+class ElectricCl extends Carcl{
+    #charge;
+    constructor(name , speed , chargePer){
+        super(name , speed);
+        this.#charge = chargePer;
+    }
+
+    accelerate(){
+        this.speed +=20;
+        console.log(`Your ${this.name} car is going at a speed of ${this.speed}`);
+        return this;
+        
+    }
+    set charging(charge){
+        this.speed--;
+        this.#charge = charge;
+        console.log(`Your charging is ${this.#charge} and going at a speed of ${this.speed}`);
+        return this;
+    }
+}
+
+const Punch = new ElectricCl('Punch',110,35);
+
+Punch.accelerate().accelerate().break().charging = 33;
+console.log(Punch);
