6b2421714bc578c1aa3290dd45ffa7fba10889df
Karthik Nandam <140495326+karthiknandam@users.noreply.github.com>
2024-09-02 23:35:49 +0530

    Delete Java Script/Forkify - JS/js directory

--- Java Script/Forkify - JS/js/config.js
+++
@@ -1,7 +0,0 @@
-export const API_KEY = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-
-export const SET_TIME = 10;
-
-export const PER_PAGE = 10;
-
-export const KEY = '4324f1e6-2ef9-461d-83a1-de1a308828d1';
