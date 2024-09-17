ae5467db07a30316b79aa1d11db3e10394300b2b
Karthik Nandam <140495326+karthiknandam@users.noreply.github.com>
2024-09-18 00:11:48 +0530

    Add files via upload

--- Java Script/DSA JS/swap_Selection_Sort.js
+++ Java Script/DSA JS/swap_Selection_Sort.js
@@ -1,16 +1,14 @@
 // we use the j value to i +1 beacuse we basicaly need the next items not the sorted items of the values;
 
 function selectionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let minIndex = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-                [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-            }
-        }
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) minIndex = j;
+      if (i !== minIndex) [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
     }
-    return arr;
+  }
+  return arr;
 }
 
 console.log(selectionSort([2, 3, 1, 5, 6]));
