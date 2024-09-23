349115fe26c0a93cfc49b9460de3364c5f4cc305
Karthik Nandam <140495326+karthiknandam@users.noreply.github.com>
2024-09-23 23:53:58 +0530

    Add files via upload

--- Java Script/DSA JS/SinglyLikedList/singlylinked.js
+++ Java Script/DSA JS/SinglyLikedList/singlylinked.js
@@ -81,8 +81,60 @@ class SinglylinkedList {
     }
     return false;
   }
+  insert(val, index) {
+    // finding the previous one value in order to search the index value which is
+    // finding the geting the index - 1 value to set the index value and next value correspoding to it:
+
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unShift(val);
+    if (index === this.length) return !!this.push(val);
+    const newNode = new node(val);
+    const value = this.get(index - 1);
+    newNode.next = value.next;
+    value.next = newNode;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return !!this.shift();
+    if (index === this.length - 1) return !!this.pop();
+    const getVal = this.get(index - 1);
+    const value = getVal.next;
+    getVal.next = value.next;
+    this.length--;
+    return value;
+  }
+  reverse() {
+    let current = this.head;
+    this.head = this.tail;
+    this.tail = current;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    return this;
+  }
+  rev() {
+    let current = this.head;
+    this.head = this.tail;
+    this.tail = current;
+    let prev = null;
+    while (current) {
+      let next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    this.head = prev;
+    return this;
+  }
 }
 const list = new SinglylinkedList();
-list.push("KART");
-list.push("Nan");
-list.push("Dam");
+list.push(1);
+list.push(2);
+list.push(3);
