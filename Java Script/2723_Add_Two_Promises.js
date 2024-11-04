053f3b7864413e60944180994119a75305be5bfe
Karthik Nandam <140495326+karthiknandam@users.noreply.github.com>
2024-11-04 23:08:29 +0530

    Delete Java Script/2723_Add_Two_Promises.js

--- Java Script/2723_Add_Two_Promises.js
+++
@@ -1,5 +0,0 @@
-var addTwoPromises = async function(promise1, promise2) {
-    const value1 = await promise1; // Wait for promise1 to resolve
-    const value2 = await promise2; // Wait for promise2 to resolve
-    return value1 + value2; // Return the sum
-};
