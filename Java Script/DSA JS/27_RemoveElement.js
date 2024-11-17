d675b9d58aa4b69b8741642a50e4280853252670
Karthik Nandam <140495326+karthiknandam@users.noreply.github.com>
2024-11-17 22:31:14 +0530

    Add files via upload

---
+++ Java Script/DSA JS/27_RemoveElement.js
@@ -0,0 +1,10 @@
+function removeElement(nums , val){
+  let k = 0 ;
+   for(let i = 0 ; i<nums.length ; i++){
+     if(nums[i] != val){
+       nums[k] = nums[i];
+       k++;
+     }
+   }
+  return k;
+}
