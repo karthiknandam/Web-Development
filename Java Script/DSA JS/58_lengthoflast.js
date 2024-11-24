79201b8d5f97381f91426032513ed0287b51b782
Karthik Nandam <140495326+karthiknandam@users.noreply.github.com>
2024-11-24 22:52:21 +0530

    Update 58_lengthoflast.js

--- Java Script/DSA JS/58_lengthoflast.js
+++ Java Script/DSA JS/58_lengthoflast.js
@@ -1,10 +1,5 @@
 var lengthOfLastWord = function(s) {
-    // Trim any trailing spaces
     s = s.trim();
-    
-    // Find the last word by splitting the string by spaces
     const words = s.split(" ");
-    
-    // Return the length of the last word
     return words[words.length - 1].length;
 };
